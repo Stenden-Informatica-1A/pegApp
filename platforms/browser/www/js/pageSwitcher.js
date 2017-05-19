55c450678d3207b9c399e48c8fe19ed3a9355434
KevinVeldman <k.p.veldman@outlook.com>
2017-05-19 08:14:33 +0200

    JQuery test

--- platforms/browser/www/js/pageSwitcher.js
+++ platforms/browser/www/js/pageSwitcher.js
@@ -1,26 +1,30 @@
-var currentPage = 1;
+var page;
+$(document).on("pagechange", function (e, data) {
+    page = data.toPage[0].id;
+});
 $(document).on('swipeleft swiperight', function (event) {
     // next page
-    if (event.type == 'swipeleft' && currentPage != 2) {
+    if (event.type == 'swipeleft' && page !="weatherDay") {
         var nextPage = $.mobile.activePage.next('[data-role=page]');
         if (nextPage) {
             $.mobile.changePage(nextPage, {
                 transition: "slide" // or any transition
             }
           );
-          ++currentPage;
         }
+        activePage = $( ".selector" ).pagecontainer( "getActivePage" );
     }
     // previous page
-    if (event.type == 'swiperight' && currentPage !=1) {
+    if (event.type == 'swiperight' && page != "main" ) {
         var prevPage = $.mobile.activePage.prev('[data-role=page]');
         if (prevPage) {
             $.mobile.changePage(prevPage, {
                 transition: "slide",
                 reverse: true // reverse effect
+
             }
           );
-          --currentPage;
+          activePage = $( ".selector" ).pagecontainer( "getActivePage" );
         }
     }
 });
